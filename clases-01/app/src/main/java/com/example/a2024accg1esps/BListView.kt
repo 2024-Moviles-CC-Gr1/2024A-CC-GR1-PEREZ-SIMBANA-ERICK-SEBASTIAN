28f2ac8198ea66c2393580ffe3a912bdde76dddd
Ericksp2001 <erick.perez01@epn.edu.ec>
2024-06-20 16:44:34 -0500

    Subida de todos los cambios

--- clases-01/app/src/main/java/com/example/a2024accg1esps/BListView.kt
+++ clases-01/app/src/main/java/com/example/a2024accg1esps/BListView.kt
@@ -1,20 +1,105 @@
 package com.example.a2024accg1esps
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class BListView : AppCompatActivity() {
+    val arreglo = BBaseDatosMemoria.arregloDBBEntrenador
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_blist_view)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_blist_view)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val listView = findViewById<ListView>(R.id.lv_list_view)
+        val adaptador = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            arreglo
+        )
+        listView.adapter = adaptador
+        adaptador.notifyDataSetChanged()
+
+        val botonAnadirListView = findViewById<Button>(R.id.btn_anadir_list_view)
+
+        botonAnadirListView.setOnClickListener {
+            anadirEntrenador(adaptador)
         }
+        registerForContextMenu(listView)
+    }
+
+    var posicionItemSeleccionado = 0
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        val info = menuInfo as AdapterView.AdapterContextMenuInfo
+        val posicion = info.position
+        posicionItemSeleccionado = posicion
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.Editar -> {
+                mostrarSnackbar("Editar $posicionItemSeleccionado")
+                return true
+            }
+            R.id.Eliminar -> {
+                mostrarSnackbar("Eliminar $posicionItemSeleccionado")
+                return true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+    fun mostrarSnackbar(texto: String) {
+        val snackbar = Snackbar.make(
+            findViewById(R.id.cl_ciclo_vida),
+            texto,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snackbar.show()
+    }
+    fun anadirEntrenador(adaptador: ArrayAdapter<BEntrenador>){
+        arreglo.add(
+            BEntrenador(4,"Jenny","d@d.com")
+        )
+        adaptador.notifyDataSetChanged()
+    }
+
+    fun abrirDialogo(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Desea Eliminar")
+        builder.setPositiveButton("Aceptar",
+            DialogInterface.OnClickListener{
+                dialogInterface, i ->
+                mostrarSnackbar("Eliminar aceptado")
+            })
+        builder.setNegativeButton("Cancelar", null)
+        val opciones = resources.getStringArray(R.array.string_array_opciones)
+        val seleccionPrevia = booleanArrayOf(true, false, false)
+        builder.setMultiChoiceItems(
+            opciones,
+            seleccionPrevia
+        ) { dialog, which, isChecked ->
+            mostrarSnackbar("Seleccionado $which")
+        }
+
+        val dialogo = builder.create()
+        dialogo.show()
     }
 }
